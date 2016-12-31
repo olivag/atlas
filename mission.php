b6bcb20c36f9180362de1dd303153d15b96121a5
Gerald Oliva <ryumaster001@yahoo.com>
2016-12-31 15:33:07 -0500

    Added context to mission.php.

--- mission.php
+++ mission.php
@@ -13,12 +13,6 @@ include("inc/header.php"); ?>
 
   <div class="main-content-top clearfix">
     <div class="content-left-of-image page-content">
-      The HVAC/R Technician training school at NTI in Maine (ME) provides technicians with the theory and knowledge required for installation, maintenance and repair of residential and light commercial systems.
-
-      The mission of the HVAC/R Training Department is to offer instruction, through a curriculum relevant to industry needs, that includes an appropriate mix of classroom and hands-on experience to bridge the gap between theory and practical application of the subjects introduced and to provide innovative, contemporary and accessible training to effectively meet student goals.  To accomplish this mission, the HVAC/R staff is committed to providing each student with a deep understanding of both fundamentals and important current issues in the heating, ventilation, air-conditioning and refrigeration industry so they may obtain productive employment or pursue advanced education.
-
-HVAC Technical Institute believes in the principle of complete preparation. Not only do we provide the essential knowledge and skills but also the opportunity for personal and professional development. Those who emerge from our program have the tools for professional growth and the essential skills needed to advance and succeed in the HVAC field.
-
       <p><strong>Atlass Institute of Technology&rsquo;s</strong> mission is to provide its students the knowledge and skills required for this field, through a curriculum relevant to industry needs, which includes a mix of hands-on and classroom training, in order to bridge the gap between theory and practical application that will allow each student to acquire a successful career and advance in the HVAC field.
       </p>
       <p><strong>Atlass Institute of Technology&rsquo;s</strong> instructors are committed to provide its students a deep understanding of the fundamentals and important current issues in the field of HVAC, so that once they graduate, they will be able to obtain employment as an HVAC technician or pursue advance education. 
@@ -36,19 +30,15 @@ HVAC Technical Institute believes in the principle of complete preparation. Not 
   <div class="content-secondary page-content">
     <p><strong>To Accomplish These Goals:</strong><br />
       <ul>
-        <li></li>
+        <li>We only accept students who display the enthusiam and have the physical ability necessary to complete this demanding course.</li>
+        <li>Solid hands-on training is given to allow students enough practice time in the lab.</li>
+        <li>Students will learn from instructors that are experts in their respective field.</li>
+        <li>Atlass Institute of Technology provides equipment that is updated regularly to meet industry standards.</li>  
+        <li>Atlass Institute of Technology offers training in English and Spanish.</li>
+        <li>Students are encourged to take part in real job sites when available, providing them with real world experience in the field.</li>
       </ul>
     </p>
   </div> <!-- end of content-secondary -->
-  To Accomplish These Goals:
-
-HVAC Technical Institute accepts only men / women who exhibit the enthusiasm and physical ability necessary to complete this rigorous course.
-HVAC Technical Institute provides the best and most updated equipment available for instruction and uses it to meet the standards within the industry.
-HVAC Technical Institute offers the complete training course in two languages (English and Spanish).
-Our most important goal is for all enrolled students to participate in real job sites when available, allowing them to work on units out in the field.
-All students will learn from videos, lab assignments and instructors who have proven field records.
-Intensive hands-on training is provided to allow students adequate time in the lab.
-Instruction on all phases of employment, including job placement assistance, customer relations and employee / employer relations will be addressed during the students training.
   
   <hr>
 
