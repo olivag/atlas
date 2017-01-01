f9e02b496383f03a93a1fd93ec63c2253622a0d9
Gerald Oliva <ryumaster001@yahoo.com>
2017-01-01 10:01:17 -0500

    Optimized images.

--- inc/footer.php
+++ inc/footer.php
@@ -64,8 +64,6 @@
 
 </div> <!-- end of main-wrapper -->
 
-<div class="testing"></div>
-
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/app.js"></script>
